a2d7e792df6e99f3ad27cc2a1ef2de7fa6e55ad8
speedydelete <speedykorok777@gmail.com>
2025-04-19 14:48:46 -0400

    new build setup v2

--- build.mjs
+++ build.mjs
@@ -1,10 +1,11 @@
 
-import {resolve} from 'path';
-import {writeFileSync} from 'fs';
+import path from 'path';
+import fs from 'fs';
+import minify from '@minify-html/node';
+import babel from '@babel/core';
 import webpack from 'webpack';
-import {minify} from 'minify';
 
-async function afterWebpack(err, stats) {
+function afterWebpack(err, stats) {
     if (err) {
         console.log('error while running webpack:\n');
         console.error(err);
@@ -13,8 +14,23 @@ async function afterWebpack(err, stats) {
     if (stats === undefined) {
         throw new Error('stats is undefined, this error should not happen');
     }
-    writeFileSync('dist/index.html', await minify('src/index.html'));
+    fs.writeFileSync('dist/index.html', minify.minify(fs.readFileSync('src/index.html'), {
+        keep_html_and_head_opening_tags: true,
+    }));
     console.log(stats.toString({colors: true}) + '\n');
+    for (let file of fs.readdirSync('dist')) {
+        if (file.endsWith('.before_babel.js')) {
+            let code = babel.transformSync(fs.readFileSync('dist/' + file), {
+                presets: ['@babel/preset-env'],
+                targets: '> 1%, not dead',
+                minified: true,
+            }).code;
+            if (code.startsWith('"use strict";(()=>{"use strict";')) {
+                code = code.slice(13);
+            }
+            fs.writeFileSync('dist/' + file.slice(0, -16) + '.js', code);
+        }
+    }
     if (!stats.hasErrors()) {
         console.log('build: complete');
     }
@@ -31,22 +47,8 @@ let compiler = webpack({
         main: './lib/index.js',
     },
     output: {
-        path: resolve(import.meta.dirname, 'dist'),
-        filename: '[name].js',
-    },
-    module: {
-        rules: [
-            {
-                exclude: /node_modules/,
-                test: /\.js$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        '@babel/preset-env',
-                    ]
-                },
-            },
-        ],
+        path: path.resolve(import.meta.dirname, 'dist'),
+        filename: '[name].before_babel.js',
     },
     devtool: mode === 'development' ? 'source-map' : undefined,
 });
